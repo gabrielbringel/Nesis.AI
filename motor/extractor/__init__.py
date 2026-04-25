9bd0a8bf312d1b444ce8fe6ead0ffef5b3c674c2
Gabriel Bringel <gbringelgoncalves@gmail.com>
2026-04-25 03:26:01 -0300

    chore: remove motor directory

--- motor/extractor/__init__.py
+++
@@ -1,4 +0,0 @@
-from motor.extractor.dose_parser import extract_dose, extract_frequency, extract_route
-from motor.extractor.ner_extractor import NERExtractor
-
-__all__ = ["NERExtractor", "extract_dose", "extract_frequency", "extract_route"]
--- motor/normalizer/__init__.py
+++
@@ -1,6 +0,0 @@
-from motor.normalizer.drug_normalizer import DrugNormalizer
-from motor.normalizer.local_anvisa_db import LocalAnvisaDB
-from motor.normalizer.pubchem_client import PubChemClient
-from motor.normalizer.rxnorm_client import RxNormClient
-
-__all__ = ["DrugNormalizer", "LocalAnvisaDB", "PubChemClient", "RxNormClient"]
