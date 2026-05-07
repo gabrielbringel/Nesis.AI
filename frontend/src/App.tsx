60fa8cf7bf05bb48bd8a013ae0df038778ac22ba
bringel <gbringelgoncalves@gmail.com>
2026-05-07 14:20:22 -0300

    refactor(app): wire reanalyzeWithData through component tree to Sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import { useSidebar } from './hooks/useSidebar'
 import { Sidebar } from './components/Sidebar'
 
 export function App() {
-  const { state, startReading, reanalyze } = useSidebar()
+  const { state, startReading, reanalyze, reanalyzeWithData } = useSidebar()
 
   return (
-    <Sidebar state={state} onStart={startReading} onReanalyze={reanalyze} />
+    <Sidebar state={state} onStart={startReading} onReanalyze={reanalyze} onReanalyzeWithData={reanalyzeWithData} />
   )
 }
